68934b511b009831fd3f764ce40f4ab428ffc0fa
jed208 <jed208@protonamil.com>
2021-03-01 11:14:47 -0700

    broken our hompage into smaller components, finished styling our menu-items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles
+import './homepage.styles.scss';
+// import components
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+)
+
+export default HomePage;
